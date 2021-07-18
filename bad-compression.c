03a2caea43e9d95e515ecca2a1ff8ab2d26fd31e
ali0gamer1 <69399548+ali0gamer1@users.noreply.github.com>
2021-07-18 17:57:06 +0430

    Update bad-compression.c

--- bad-compression.c
+++ bad-compression.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <limits.h>
 
-
-struct linkedlist{
-    short repeat;
-    char val;
-    struct linkedlist *next;
-};
-
-
 struct linkedlist *addtolist(struct linkedlist *root, char c, short count)
 {
+
+
+
     if(root == NULL)
     {
         root = (struct linkedlist*) malloc(sizeof(struct linkedlist));
+
         root->repeat = count;
         root->val = c;
         root->next = NULL;
@@ -27,6 +23,50 @@ struct linkedlist *addtolist(struct linkedlist *root, char c, short count)
 
 }
 
+int counter = 0;
+
+struct linkedlist *strtotree(char *str)
+{
+    int _strlen = strlen(str) + 1;
+    struct linkedlist *root = NULL;
+
+    if (_strlen <= 1)
+    {
+        return NULL;
+    }
+
+    char c = *str;
+    short count = 1;
+
+    for (int i = 1;i < _strlen;i++)
+    {
+        if (str[i] == c)
+        {
+            if (count == SHRT_MAX)
+            {
+                counter++;
+                root = addtolist(root, c, count);
+                c = str[i];
+                count = 0;
+            }
+            count++;
+        }
+        else
+        {
+            counter++;
+            root = addtolist(root, c, count);
+            c = str[i];
+            count = 1;
+        }
+
+    }
+
+
+
+    return root;
+
+}
+
 void print( struct linkedlist  *head) {
 
     struct linkedlist  *current_node = head;
@@ -35,7 +75,9 @@ void print( struct linkedlist  *head) {
         for(int i = 0 ;i < current_node->repeat;i++)
         {
             putchar(current_node->val);
+
         }
+
         current_node = current_node->next;
     }
 }
